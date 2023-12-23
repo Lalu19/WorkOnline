34f9f17c3eeac89db27f6ddc249f6ffca7ba9b6a
Trinatha kumar sethy <trinathsethy872@gmail.com>
2023-12-23 18:13:59 +0530

    Validation add in district mapping

---
+++ Library/CloudVOffice.Services/WareHouses/Districts/SpecificException.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace CloudVOffice.Services.WareHouses.Districts
+{
+    [Serializable]
+    internal class SpecificException : Exception
+    {
+        public SpecificException()
+        {
+        }
+
+        public SpecificException(string? message) : base(message)
+        {
+        }
+
+        public SpecificException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected SpecificException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
