6925a32b8b57425cf364d79e4fca66ad8d31467b
Trinatha kumar sethy <trinathsethy872@gmail.com>
2023-12-23 15:01:38 +0530

    Month crud

---
+++ Library/CloudVOffice.Services/WareHouses/Months/MonthService.cs
@@ -0,0 +1,122 @@
+﻿using CloudVOffice.Core.Domain.Common;
+using CloudVOffice.Core.Domain.WareHouses.Districts;
+using CloudVOffice.Core.Domain.WareHouses.Months;
+using CloudVOffice.Data.DTO.WareHouses.Districts;
+using CloudVOffice.Data.DTO.WareHouses.Months;
+using CloudVOffice.Data.Persistence;
+using CloudVOffice.Data.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudVOffice.Services.WareHouses.Months
+{
+    public class MonthService: IMonthService
+    {
+        private readonly ApplicationDBContext _dbContext;
+        private readonly ISqlRepository<Month> _monthRepo;
+
+        public MonthService(ApplicationDBContext dbContext,
+                                   ISqlRepository<Month> monthRepo
+
+                                    )
+        {
+            _dbContext = dbContext;
+            _monthRepo = monthRepo;
+
+        }
+        public MessageEnum MonthCreate(MonthDTO monthDTO)
+        {
+            try
+            {
+                var objcheck = _dbContext.Months.SingleOrDefault(opt => opt.Deleted == false && opt.MonthName == monthDTO.MonthName);
+                if (objcheck == null)
+                {
+                    Month sc = new Month();
+                    sc.MonthName = monthDTO.MonthName;
+                    sc.CreatedBy = monthDTO.CreatedBy;
+                    sc.CreatedDate = System.DateTime.Now;
+                    var obj = _monthRepo.Insert(sc);
+                    return MessageEnum.Success;
+                }
+                else
+                {
+                    return MessageEnum.Duplicate;
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        public List<Month> GetMonthList()
+        {
+            try
+            {
+                return _dbContext.Months.Where(x => x.Deleted == false).ToList();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        public MessageEnum MonthUpdate(MonthDTO monthDTO)
+        {
+            try
+            {
+                var updateMonth = _dbContext.Months.Where(x => x.MonthId != monthDTO.MonthId && x.MonthName == monthDTO.MonthName && x.Deleted == false).FirstOrDefault();
+                if (updateMonth == null)
+                {
+                    var a = _dbContext.Months.Where(x => x.MonthId == monthDTO.MonthId).FirstOrDefault();
+                    if (a != null)
+                    {
+                        a.MonthName = monthDTO.MonthName;
+                        a.UpdatedBy = monthDTO.CreatedBy;
+                        a.UpdatedDate = DateTime.Now;
+                        _dbContext.SaveChanges();
+                        return MessageEnum.Updated;
+                    }
+                    else
+                        return MessageEnum.Invalid;
+                }
+                else
+                {
+                    return MessageEnum.Duplicate;
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        public Month GetMonthById(Int64 MonthId)
+        {
+            return _dbContext.Months.Where(x => x.MonthId == MonthId && x.Deleted == false).SingleOrDefault();
+        }
+
+        public MessageEnum MonthDelete(Int64 MonthId, Int64 DeletedBy)
+        {
+            try
+            {
+                var a = _dbContext.Months.Where(x => x.MonthId == MonthId).FirstOrDefault();
+
+                if (a != null)
+                {
+                    a.Deleted = true;
+                    a.UpdatedBy = DeletedBy;
+                    a.UpdatedDate = DateTime.Now;
+                    _dbContext.SaveChanges();
+                    return MessageEnum.Deleted;
+                }
+                else
+                    return MessageEnum.Invalid;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ Plugin/Warehouse.Management/Controllers/MonthController.cs
@@ -0,0 +1,114 @@
+﻿using CloudVOffice.Core.Domain.Common;
+using CloudVOffice.Core.Domain.WareHouses.Districts;
+using CloudVOffice.Core.Domain.WareHouses.Months;
+using CloudVOffice.Data.DTO.WareHouses.Districts;
+using CloudVOffice.Data.DTO.WareHouses.Months;
+using CloudVOffice.Services.WareHouses.Districts;
+using CloudVOffice.Services.WareHouses.Months;
+using CloudVOffice.Web.Framework;
+using CloudVOffice.Web.Framework.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse.Management.Controllers
+{
+    [Area(AreaNames.WareHouse)]
+    public class MonthController : BasePluginController
+    {
+        private readonly IMonthService _monthService;
+
+        public MonthController(IMonthService monthService)
+        {
+            _monthService = monthService;
+
+        }
+        [HttpGet]
+        public IActionResult MonthCreate(Int64? MonthId)
+        {
+
+
+            MonthDTO monthDTO = new MonthDTO();
+            if (MonthId != null)
+            {
+
+                Month d = _monthService.GetMonthById(int.Parse(MonthId.ToString()));
+
+                monthDTO.MonthName = d.MonthName;
+            }
+            return View("~/Plugins/Warehouse.Management/Views/Month/MonthCreate.cshtml", monthDTO);
+        }
+
+        [HttpPost]
+        public IActionResult MonthCreate(MonthDTO monthDTO)
+        {
+            monthDTO.CreatedBy = Int64.Parse(User.Claims.FirstOrDefault(x => x.Type == "UserId").Value.ToString());
+
+            if (ModelState.IsValid)
+            {
+                if (monthDTO.MonthId == null)
+                {
+                    var a = _monthService.MonthCreate(monthDTO);
+
+                    if (a == MessageEnum.Success)
+                    {
+                        TempData["msg"] = MessageEnum.Success;
+                        return Redirect("/WareHouse/Month/MonthView");
+                    }
+                    else if (a == MessageEnum.Duplicate)
+                    {
+                        TempData["msg"] = MessageEnum.Duplicate;
+                        ModelState.AddModelError("", "Month Already Exists");
+                    }
+                    else
+                    {
+                        TempData["msg"] = MessageEnum.UnExpectedError;
+                        ModelState.AddModelError("", "Un-Expected Error");
+                    }
+                }
+                else
+                {
+                    var a = _monthService.MonthUpdate(monthDTO);
+                    if (a == MessageEnum.Updated)
+                    {
+                        TempData["msg"] = MessageEnum.Updated;
+                        return Redirect("/WareHouse/Month/MonthView");
+
+                    }
+                    else if (a == MessageEnum.Duplicate)
+                    {
+                        TempData["msg"] = MessageEnum.Duplicate;
+                        ModelState.AddModelError("", "Month Already Exists");
+                    }
+                    else
+                    {
+                        TempData["msg"] = MessageEnum.UnExpectedError;
+                        ModelState.AddModelError("", "Un-Expected Error");
+                    }
+                }
+            }
+
+            return View("~/Plugins/Warehouse.Management/Views/Month/MonthCreate.cshtml", monthDTO);
+
+        }
+
+        public IActionResult MonthView()
+        {
+            ViewBag.Months = _monthService.GetMonthList();
+
+            return View("~/Plugins/Warehouse.Management/Views/Month/MonthView.cshtml");
+        }
+
+        [HttpGet]
+        public IActionResult MonthDelete(Int64 MonthId)
+        {
+            Int64 DeletedBy = Int64.Parse(User.Claims.FirstOrDefault(x => x.Type == "UserId").Value.ToString());
+
+            var a = _monthService.MonthDelete(MonthId, DeletedBy);
+            return Redirect("/WareHouse/Month/MonthView");
+        }
+    }
+}
