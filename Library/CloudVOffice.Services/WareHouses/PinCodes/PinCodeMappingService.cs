ffdcc627e1df04f1bc3ad0ed4d48b21436dce41f
Trinatha kumar sethy <trinathsethy872@gmail.com>
2023-12-27 16:44:47 +0530

    Valiadation will apply on  District mapping and pin code mapping

--- Library/CloudVOffice.Services/WareHouses/PinCodes/PinCodeMappingService.cs
+++ Library/CloudVOffice.Services/WareHouses/PinCodes/PinCodeMappingService.cs
@@ -1,8 +1,11 @@
 ﻿using CloudVOffice.Core.Domain.Common;
+using CloudVOffice.Core.Domain.WareHouses.Districts;
 using CloudVOffice.Core.Domain.WareHouses.PinCodes;
+using CloudVOffice.Data.DTO.WareHouses.Districts;
 using CloudVOffice.Data.DTO.WareHouses.PinCodes;
 using CloudVOffice.Data.Persistence;
 using CloudVOffice.Data.Repository;
+using CloudVOffice.Services.WareHouses.Districts;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -27,88 +30,191 @@ namespace CloudVOffice.Services.WareHouses.PinCodes
 
 		}
 
-		public MessageEnum PinCodeMappingCreate(PinCodeMappingDTO pinCodeMappingDTO)
-		{
-			try
-			{
-				//var objcheck = _dbContext.PinCodeMappings.Where(x => x.Deleted == false).FirstOrDefault();
-				var objcheck = _dbContext.PinCodeMappings.FirstOrDefault(opt => opt.Deleted == false && opt.WareHuoseId == pinCodeMappingDTO.WareHuoseId);
+        //public MessageEnum PinCodeMappingCreate(PinCodeMappingDTO pinCodeMappingDTO)
+        //{
+        //	try
+        //	{
+        //		//var objcheck = _dbContext.PinCodeMappings.Where(x => x.Deleted == false).FirstOrDefault();
+        //		var objcheck = _dbContext.PinCodeMappings.FirstOrDefault(opt => opt.Deleted == false && opt.WareHuoseId == pinCodeMappingDTO.WareHuoseId);
 
-				if (objcheck == null)
-				{
-					foreach (var pinmappingid in pinCodeMappingDTO.PinCodeId)
-					{
-						PinCodeMapping pinCodeMapping = new PinCodeMapping();
+        //		if (objcheck == null)
+        //		{
+        //			foreach (var pinmappingid in pinCodeMappingDTO.PinCodeId)
+        //			{
+        //				PinCodeMapping pinCodeMapping = new PinCodeMapping();
 
-						pinCodeMapping.PinCodeId = pinmappingid;
-						pinCodeMapping.WareHuoseId = pinCodeMappingDTO.WareHuoseId;
-						pinCodeMapping.CreatedBy = pinCodeMappingDTO.CreatedBy;
-						pinCodeMapping.CreatedDate = System.DateTime.Now;
-						var obj = _pinCodeMappingRepo.Insert(pinCodeMapping);
+        //				pinCodeMapping.PinCodeId = pinmappingid;
+        //				pinCodeMapping.WareHuoseId = pinCodeMappingDTO.WareHuoseId;
+        //				pinCodeMapping.CreatedBy = pinCodeMappingDTO.CreatedBy;
+        //				pinCodeMapping.CreatedDate = System.DateTime.Now;
+        //				var obj = _pinCodeMappingRepo.Insert(pinCodeMapping);
 
-					}
-					return MessageEnum.Success;
-				}
-				else if (objcheck != null)
-				{
-					return MessageEnum.Duplicate;
-				}
+        //			}
+        //			return MessageEnum.Success;
+        //		}
+        //		else if (objcheck != null)
+        //		{
+        //			return MessageEnum.Duplicate;
+        //		}
 
-				return MessageEnum.UnExpectedError;
-			}
-			catch
-			{
-				throw;
-			}
-		}
+        //		return MessageEnum.UnExpectedError;
+        //	}
+        //	catch
+        //	{
+        //		throw;
+        //	}
+        //}
+        public MessageEnum PinCodeMappingCreate(PinCodeMappingDTO pinCodeMappingDTO)
+        {
+            try
+            {
+                var pincodeExists = _dbContext.PinCodeMappings.Any(opt => opt.Deleted == false && opt.WareHuoseId == pinCodeMappingDTO.WareHuoseId);
 
+                if (!pincodeExists)
+                {
+                    foreach (var pinCodeMappingId in pinCodeMappingDTO.PinCodeId)
+                    {
+                        var pinCodeExists = _dbContext.PinCodeMappings.Any(opt => opt.Deleted == false && opt.PinCodeId == pinCodeMappingId);
 
+                        if (!pinCodeExists)
+                        {
+                            PinCodeMapping pinCodeMapping = new PinCodeMapping
+                            {
+                                PinCodeId = pinCodeMappingId,
+                                WareHuoseId = (long)pinCodeMappingDTO.WareHuoseId,
+                                CreatedBy = pinCodeMappingDTO.CreatedBy,
+                                CreatedDate = DateTime.UtcNow // Use UTC time
+                            };
 
-		public List<PinCodeMapping> GetPinCodeMappingList()
+                            _pinCodeMappingRepo.Insert(pinCodeMapping);
+                        }
+                        else
+                        {
+                            return MessageEnum.Duplicate;
+                        }
+                    }
+
+                    return MessageEnum.Success;
+                }
+                else
+                {
+                    return MessageEnum.Duplicate;
+                }
+            }
+            catch (SpecificException ex)
+            {
+                // Handle specific exception
+                return MessageEnum.UnExpectedError;
+            }
+        }
+
+        public List<PinCodeMapping> GetPinCodeMappingList()
 		{
 			return _dbContext.PinCodeMappings
 				.Include(s => s.WareHuose)
 				.Include(s => s.PinCode)
 				.Where(x => x.Deleted == false).ToList();
 		}
-		public MessageEnum PinCodeMappingUpdate(PinCodeMappingDTO pinCodeMappingDTO)
-		{
-			try
-			{
-				var updatepinCodeMapping = _dbContext.PinCodeMappings.Where(x => x.PinCodeMappingId != pinCodeMappingDTO.PinCodeMappingId && pinCodeMappingDTO.PinCodeId.Contains(x.PinCodeId) && x.WareHuoseId == pinCodeMappingDTO.WareHuoseId && x.Deleted == false).FirstOrDefault();
+        //public MessageEnum PinCodeMappingUpdate(PinCodeMappingDTO pinCodeMappingDTO)
+        //{
+        //	try
+        //	{
+        //		var updatepinCodeMapping = _dbContext.PinCodeMappings.Where(x => x.PinCodeMappingId != pinCodeMappingDTO.PinCodeMappingId && pinCodeMappingDTO.PinCodeId.Contains(x.PinCodeId) && x.WareHuoseId == pinCodeMappingDTO.WareHuoseId && x.Deleted == false).FirstOrDefault();
 
-				if (updatepinCodeMapping == null)
-				{
-					var a = _dbContext.PinCodeMappings.Where(x => x.PinCodeMappingId == pinCodeMappingDTO.PinCodeMappingId).FirstOrDefault();
-					if (a != null)
-					{
-						foreach (var pinmappingid in pinCodeMappingDTO.PinCodeId)
-						{
-							a.WareHuoseId = pinCodeMappingDTO.WareHuoseId;
-							a.PinCodeId = pinmappingid;
-							
-							a.UpdatedBy = pinCodeMappingDTO.CreatedBy;
-							a.UpdatedDate = DateTime.Now;
-							_dbContext.SaveChanges();
+        //		if (updatepinCodeMapping == null)
+        //		{
+        //			var a = _dbContext.PinCodeMappings.Where(x => x.PinCodeMappingId == pinCodeMappingDTO.PinCodeMappingId).FirstOrDefault();
+        //			if (a != null)
+        //			{
+        //				foreach (var pinmappingid in pinCodeMappingDTO.PinCodeId)
+        //				{
+        //					a.WareHuoseId = pinCodeMappingDTO.WareHuoseId;
+        //					a.PinCodeId = pinmappingid;
 
-						}
-						return MessageEnum.Updated;
-					}
-					else
-						return MessageEnum.Invalid;
-				}
-				else
-				{
-					return MessageEnum.Duplicate;
-				}
+        //					a.UpdatedBy = pinCodeMappingDTO.CreatedBy;
+        //					a.UpdatedDate = DateTime.Now;
+        //					_dbContext.SaveChanges();
 
-			}
-			catch
-			{
-				throw;
-			}
-		}
-		public PinCodeMapping GetPinCodeMappingById(Int64 PinCodeMappingId)
+        //				}
+        //				return MessageEnum.Updated;
+        //			}
+        //			else
+        //				return MessageEnum.Invalid;
+        //		}
+        //		else
+        //		{
+        //			return MessageEnum.Duplicate;
+        //		}
+
+        //	}
+        //	catch
+        //	{
+        //		throw;
+        //	}
+        //}
+
+        public MessageEnum PinCodeMappingUpdate(PinCodeMappingDTO pinCodeMappingDTO)
+        {
+            try
+            {
+                
+                var duplicateCheck = _dbContext.PinCodeMappings
+                    .Any(x => x.PinCodeMappingId != pinCodeMappingDTO.PinCodeMappingId &&
+                              pinCodeMappingDTO.PinCodeId.Contains(x.PinCodeId) &&
+                              x.WareHuoseId == pinCodeMappingDTO.WareHuoseId &&
+                              !x.Deleted);
+
+                if (!duplicateCheck)
+                {
+                    var existingMappings = _dbContext.PinCodeMappings
+                        .Where(x => x.PinCodeMappingId == pinCodeMappingDTO.PinCodeMappingId);
+
+                    
+                    _dbContext.PinCodeMappings.RemoveRange(existingMappings);
+
+                   
+                    foreach (var newPinCodeId in pinCodeMappingDTO.PinCodeId)
+                    {
+                        
+                        var existingMapping = _dbContext.PinCodeMappings
+                            .FirstOrDefault(x => x.PinCodeId == newPinCodeId && x.WareHuoseId != pinCodeMappingDTO.WareHuoseId && !x.Deleted);
+
+                        if (existingMapping == null)
+                        {
+                            PinCodeMapping newMapping = new PinCodeMapping
+                            {
+                                WareHuoseId = (long)pinCodeMappingDTO.WareHuoseId,
+                                PinCodeId = newPinCodeId,
+                                CreatedBy = pinCodeMappingDTO.CreatedBy,
+                                CreatedDate = DateTime.Now
+                            };
+
+                            _dbContext.PinCodeMappings.Add(newMapping);
+                        }
+                        else
+                        {
+                            
+                            return MessageEnum.Duplicate;
+                        }
+                    }
+
+                    _dbContext.SaveChanges();
+
+                    return MessageEnum.Updated;
+                }
+                else
+                {
+                    return MessageEnum.Duplicate;
+                }
+            }
+            catch (Exception ex)
+            {
+                
+                throw;
+            }
+        }
+
+        public PinCodeMapping GetPinCodeMappingById(Int64 PinCodeMappingId)
 		{
 			return _dbContext.PinCodeMappings.Where(x => x.PinCodeMappingId == PinCodeMappingId && x.Deleted == false).SingleOrDefault();
 		}
