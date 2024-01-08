df2d4d1dd9fb83f722612be6118213e53fbe5542
pritam.sahoo <pritam.sahoo@appman.tech>
2024-01-08 10:46:56 +0530

    In dto  nullable is added

--- Library/CloudVOffice.Data/DTO/ProductCategories/SubCategory4DTO.cs
+++ Library/CloudVOffice.Data/DTO/ProductCategories/SubCategory4DTO.cs
@@ -9,11 +9,11 @@ namespace CloudVOffice.Data.DTO.ProductCategories
 	public class SubCategory4DTO
 	{
 		public int? SubCategory4Id { get; set; }
-		public int SectorId { get; set; }
-		public int CategoryId { get; set; }
-		public int SubCategory1Id { get; set; }
-		public int SubCategory2Id { get; set; }
-		public int SubCategory3Id { get; set; }
+		public int? SectorId { get; set; }
+		public int? CategoryId { get; set; }
+		public int? SubCategory1Id { get; set; }
+		public int? SubCategory2Id { get; set; }
+		public int? SubCategory3Id { get; set; }
 		public string SubCategory4Name { get; set; }
 		public Int64 CreatedBy { get; set; }
 	}
