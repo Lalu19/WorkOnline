d3f34e2bac77970445db02bdce5a63096582af63
Lalu19 <laluprasadd99@gmail.com>
2024-01-27 12:38:19 +0530

    Image controller added

--- Plugin/Web.API/Controllers/Buyers/BuyerRegistrationController.cs
+++ Plugin/Web.API/Controllers/Buyers/BuyerRegistrationController.cs
@@ -1,7 +1,10 @@
-﻿using CloudVOffice.Data.DTO.Buyers;
+﻿using CloudVOffice.Core.Domain.Buyers;
+using CloudVOffice.Data.DTO.Buyers;
 using CloudVOffice.Data.DTO.ProductCategories;
 using CloudVOffice.Services.Buyers;
 using CloudVOffice.Services.ProductCategories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -16,23 +19,68 @@ namespace Web.API.Controllers.Buyers
     public class BuyerRegistrationController : Controller
     {
         private readonly IBuyerRegistrationService _buyerRegistrationService;
-        public BuyerRegistrationController(IBuyerRegistrationService buyerRegistrationService)
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        public BuyerRegistrationController(IBuyerRegistrationService buyerRegistrationService, IWebHostEnvironment hostingEnvironment)
         {
             _buyerRegistrationService = buyerRegistrationService;
+            _hostingEnvironment = hostingEnvironment;
         }
+        //[HttpPost]
+        //public IActionResult BuyerSigninCreate(BuyerRegistrationDTO buyerRegistrationDTO)
+        //{
+        //    try
+        //    {
+        //        var a = _buyerRegistrationService.CreateBuyerRegistration(buyerRegistrationDTO);
+        //        return Ok(a);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return Accepted(new { Status = "error", ResponseMsg = ex.Message });
+        //    }
+        //}
+
         [HttpPost]
-        public IActionResult BuyerSigninCreate(BuyerRegistrationDTO buyerRegistrationDTO)
+        public IActionResult BuyerSigninCreate([FromForm] BuyerRegistrationDTO buyerRegistrationDTO)
         {
-            try
+           
+            if (buyerRegistrationDTO.imageUpload != null)
             {
-                var a = _buyerRegistrationService.CreateBuyerRegistration(buyerRegistrationDTO);
-                return Ok(a);
+                FileInfo fileInfo = new FileInfo(buyerRegistrationDTO.imageUpload.FileName);
+                string extn = fileInfo.Extension.ToLower();
+                Guid id = Guid.NewGuid();
+                string filename = id.ToString() + Path.GetExtension(buyerRegistrationDTO.imageUpload.FileName);
+
+                //string filename = id.ToString() + extn;
+                string newpath = DateTime.Today.Date.ToString("dd-MMM-yyyy");
+                string uploadsFolder = Path.Combine(_hostingEnvironment.WebRootPath, @"uploads\BuyerSnaps\" + buyerRegistrationDTO.CreatedBy.ToString());
+                if (!Directory.Exists(uploadsFolder))
+                {
+                    Directory.CreateDirectory(uploadsFolder);
+                }
+                string uniqueFileName = Guid.NewGuid().ToString() + "_" + filename;
+                string imagePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                // Copy the uploaded image to the specified path
+                using (var stream = new FileStream(imagePath, FileMode.Create))
+                {
+                    buyerRegistrationDTO.imageUpload.CopyTo(stream);
+                }
+
+                buyerRegistrationDTO.imageUpload.CopyTo(new FileStream(imagePath, FileMode.Create));
+                //buyerRegistrationDTO.ShopImage = Path.Combine("uploads", "buyerregistrations", buyerRegistrationDTO.CreatedBy.ToString(), filename);
+                buyerRegistrationDTO.ShopImage = filename;
+                //if (fileInfo.Exists)
+                //{
+                //    buyerRegistrationDTO.FileSize = fileInfo.Length;
+                //}
+
             }
-            catch (Exception ex)
-            {
-                return Accepted(new { Status = "error", ResponseMsg = ex.Message });
-            }
+            var a = _buyerRegistrationService.CreateBuyerRegistration(buyerRegistrationDTO);
+
+
+            return Ok(a);
         }
+
         [HttpGet]
         public IActionResult BuyerSigninList()
         {
