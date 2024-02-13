6a9969a1d896b9fe83ab694513ae9c9387b1102d
Lalu19 <laluprasadd99@gmail.com>
2024-02-13 11:19:23 +0530

    in api images are showed in array format

---
+++ Plugin/Web.API/Controllers/PincodeController.cs
@@ -0,0 +1,54 @@
+﻿using CloudVOffice.Data.DTO.WareHouses;
+using CloudVOffice.Data.DTO.WareHouses.PinCodes;
+using CloudVOffice.Services.WareHouse.PinCodes;
+using CloudVOffice.Services.WareHouses;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PincodeController : Controller
+    {
+        private readonly IPinCodeService _pincodeService;
+        public PincodeController(IPinCodeService pincodeService)
+        {
+            _pincodeService = pincodeService;
+        }
+        [HttpPost]
+        public IActionResult PincodeCreate(PinCodeDTO pinCodeDTO)
+        {
+            try
+            {
+                var a = _pincodeService.PinCodeCreate(pinCodeDTO);
+                return Ok(a);
+            }
+            catch (Exception ex)
+            {
+                return Accepted(new { Status = "error", ResponseMsg = ex.Message });
+            }
+        }
+
+        [HttpGet]
+        public IActionResult PIncodeList()
+        {
+            var a = _pincodeService.GetPinList();
+            return Ok(a);
+        }
+
+        [HttpGet("{pincodeId}")]
+        public IActionResult SinglePincodeListbyId(Int64 pincodeId)
+        {
+            var a = _pincodeService.GetPinByPinCodeId(pincodeId);
+            return Ok(a);
+        }
+
+
+
+    }
+}
---
+++ Plugin/Web.API/Controllers/WareHouse/WareHouseController.cs
@@ -0,0 +1,50 @@
+﻿using CloudVOffice.Data.DTO.WareHouses;
+using CloudVOffice.Data.DTO.WareHouses.Brands;
+using CloudVOffice.Services.WareHouses;
+using CloudVOffice.Services.WareHouses.Itemss;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web.API.Controllers.WareHouse
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class WareHouseController : Controller
+    {
+        private readonly IWareHouseService _warehouseService;
+        public WareHouseController(IWareHouseService warehouseService)
+        {
+            _warehouseService = warehouseService;
+        }
+        [HttpPost]
+        public IActionResult WareHouseCreate(WareHouseDTO wareHouseDTO)
+        {
+            try
+            {
+                var a = _warehouseService.WareHouseCreate(wareHouseDTO);
+                return Ok(a);
+            }
+            catch (Exception ex)
+            {
+                return Accepted(new { Status = "error", ResponseMsg = ex.Message });
+            }
+        }
+        [HttpGet]
+        public IActionResult WareHouseList()
+        {
+            var a = _warehouseService.GetWareHouseList();
+            return Ok(a);
+        }
+        [HttpGet("{warehouseId}")]
+        public IActionResult SingleWareHouseListbyId(Int64 warehouseId)
+        {
+            var a = _warehouseService.GetWareHousebyWareHuoseId(warehouseId);
+            return Ok(a);
+        }
+
+    }
+}
