315f66d916c04d57e49ea6ee3cae5b08ad1e29ca
pritam.sahoo <pritam.sahoo@appman.tech>
2024-02-23 09:57:02 +0530

    DistributorPartner

---
+++ Presentation/DistributorPartner/Model/DistributorUsers/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace DistributorPartner.Model.DistributorUsers
+{
+    public partial record LoginModel
+    {
+        [DataType(DataType.PhoneNumber)]
+        [DisplayName("Mobile Number")]
+        public string UserMobileNumber { get; set; }
+
+        [DataType(DataType.Password)]
+        [DisplayName("Password")]
+        public string Password { get; set; }
+    }
+}
