998184cdcb24aba978994e49d7d6677f511ccad5
Bipin Bihari Parida <bipinparida111@gmail.com>
2024-03-13 15:50:51 +0530

    Sales Admin Modify

--- Library/CloudVOffice.Data/DTO/Sales/SalesAdminDTO.cs
+++ Library/CloudVOffice.Data/DTO/Sales/SalesAdminDTO.cs
@@ -13,11 +13,15 @@ namespace CloudVOffice.Data.DTO.Sales
 		public Int64? SalesManagerId { get; set; }
 		public string? SalesAdminTargetName { get; set; }
         public Int64 MonthId { get; set; }
-        public Int64? SectorId { get; set; }
-		public Int64? CategoryId { get; set; }
-		public Int64? BrandId { get; set; }
-		public string? Sector { get; set; }
-		public string? Category { get; set; }
+
+        //public Int64? SectorId { get; set; }
+        //public Int64? CategoryId { get; set; }
+        public int? SectorId { get; set; }
+        public int? CategoryId { get; set; }
+        public Int64? BrandId { get; set; }
+
+		//public string? Sector { get; set; }
+		//public string? Category { get; set; }
 		public double? MonthlyCategoryWiseTarget { get; set; }
 		public double? MonthlySectorWiseTarget { get; set; }
 		public double? MonthlyBrandWiseTarget { get; set; }
